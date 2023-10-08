4ab42a544bf560a171c695b73cbc45353dff2efa
Adeyinka Junaid <yinkajunn@gmail.com>
2023-10-08 23:35:19 +0100

    Add zustand and Modal components and Logic

--- components/Input.tsx
+++ components/Input.tsx
@@ -2,13 +2,20 @@
 interface InputProps {
     placeholder?: string
     value?: string
+    type?: string
+    disabled?: boolean
+    onChange: (event: React.ChangeEvent<HTMLInputElement>) => void
 }
 
-const Input = () => {
+const Input: React.FC<InputProps> = ({ placeholder, value, type, disabled, onChange }) => {
     return (
-        <div>
-
-        </div>
+        <input 
+        disabled={disabled}
+        onChange={onChange}
+        placeholder={placeholder}
+        type={type}
+        className=" w-full p-6 text-lg bg-black border-2 border-neutral-800 rounded-md outline-none text-white focus:border-sky-500 focus:border-2"
+         />
     )
 }
 
--- components/Modal.tsx
+++ components/Modal.tsx
@@ -34,7 +34,7 @@ const Modal: React.FC<ModalProps> = ({ isOpen, onClose, onSubmit, title, body, f
 
   return (
     <>
-      <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none bg-neutral-800 bg-opacity-70">
+      <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none bg-neutral-500 bg-opacity-70">
         <div className="relative w-full lg:w-3/6 my-6 h-full mx-auto lg:h-auto">
           {/* Content */}
           <div className="h-full lg:h-auto border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-black outline-none focus:outline-none">
